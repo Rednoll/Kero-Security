5bd0fdf697fa2b8e9124037f7e782c6d7958be80
Kerovazzky <rednoll12@gmail.com>
2020-10-12 22:34:51 +0300

    add Lexer test

--- src/test/java/com/kero/security/lang/lexems/DefaultRuleLexemTest.java
+++ src/test/java/com/kero/security/lang/lexems/DefaultRuleLexemTest.java
@@ -1,19 +1,34 @@
 package com.kero.security.lang.lexems;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import com.kero.security.lang.tokens.DefaultRuleToken;
+
 public class DefaultRuleLexemTest {
 
 	@Test
-	public void test() {
+	public void match() {
 		
 		DefaultRuleLexem lexem = new DefaultRuleLexem();
 		
 		assertTrue(lexem.isMatch("(G)"));
 		assertTrue(lexem.isMatch("(D)"));
 		assertFalse(lexem.isMatch("(C)"));
+	}
+	
+	@Test
+	public void tokenize() {
+		
+		DefaultRuleLexem lexem = new DefaultRuleLexem();
+		
+		DefaultRuleToken grant = lexem.tokenize("(G)");
+		assertEquals(grant, DefaultRuleToken.GRANT);
+
+		DefaultRuleToken deny = lexem.tokenize("(D)");
+		assertEquals(deny, DefaultRuleToken.DENY);
 	}
 }
