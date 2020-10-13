440bcebad1e42927b0d7ca99aa15dd6a69201aa5
Kerovazzky <rednoll12@gmail.com>
2020-10-13 03:20:55 +0300

    add AccessSchemeTest

---
+++ src/test/java/com/kero/security/core/scheme/AccessSchemeTest.java
@@ -0,0 +1,54 @@
+package com.kero.security.core.scheme;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+
+import com.kero.security.core.access.Access;
+import com.kero.security.core.property.Property;
+
+public class AccessSchemeTest {
+
+	@Test
+	public void verifyEmpty() {
+		
+		AccessScheme empty = AccessScheme.EMPTY;
+		
+		assertEquals(empty.collectProperties(), Collections.emptySet());
+		assertEquals(empty.createLocalProperty(""), Property.EMPTY);
+		assertEquals(empty.determineDefaultAccess(), Access.UNKNOWN);
+		assertEquals(empty.getAgent(), null);
+		assertEquals(empty.getDefaultAccess(), Access.UNKNOWN);
+		assertEquals(empty.getLocalProperties(), Collections.EMPTY_SET);
+		assertEquals(empty.getLocalProperty(""), Property.EMPTY);
+		assertEquals(empty.getName(), null);
+		assertEquals(empty.getOrCreateLocalProperty(""), Property.EMPTY);
+		assertEquals(empty.getParent(), AccessScheme.EMPTY);
+		assertEquals(empty.getParentProperty(""), Property.EMPTY);
+		assertEquals(empty.getTypeClass(), null);
+	}
+	
+	@Test
+	public void getOrCreateLocalProperty() {
+		
+		AccessScheme scheme = new ClassAccessScheme();
+		
+		Property created = scheme.createLocalProperty("name");
+		
+		Property getted = scheme.getOrCreateLocalProperty("name");
+	
+		assertTrue(created == getted);
+	}
+	
+	@Test
+	public void addCacheWrap() {
+		
+		AccessScheme wrap = AccessScheme.addCacheWrap(AccessScheme.EMPTY);
+		AccessScheme wrap2 = AccessScheme.addCacheWrap(wrap);
+	
+		assertTrue(wrap == wrap2);
+	}
+}
