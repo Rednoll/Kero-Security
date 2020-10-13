18e6ba613478371936848692fbf2c7e07eef268a
Kerovazzky <rednoll12@gmail.com>
2020-10-13 03:08:07 +0300

    fix FileResource test

--- src/test/java/com/kero/security/lang/provider/resource/FileResourceTest.java
+++ src/test/java/com/kero/security/lang/provider/resource/FileResourceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
@@ -19,8 +20,21 @@ public class FileResourceTest {
 		Path file1 = Files.createTempFile(dir, "file1", ".k-s");
 		Path file2 = Files.createTempFile(dir, "file2", ".k-s");
 		
-		Files.delete(file1);
-		Files.delete(file2);
+		try {
+			
+			Files.delete(file1);
+		}
+		catch(NoSuchFileException e) {
+			
+		}
+		
+		try {
+
+			Files.delete(file2);
+		}
+		catch(NoSuchFileException e) {
+			
+		}
 		
 		Files.write(file1, "test1".getBytes(), StandardOpenOption.CREATE_NEW);
 		Files.write(file2, "test2".getBytes(), StandardOpenOption.CREATE_NEW);
