bdef8f9aed8bf1b863307a52fd654ad7a20df2af
Kerovazzky <rednoll12@gmail.com>
2020-10-23 23:10:52 +0300

    make ProviderCacheWrap preloadable

--- src/main/java/com/kero/security/lang/provider/ProviderCacheWrap.java
+++ src/main/java/com/kero/security/lang/provider/ProviderCacheWrap.java
@@ -2,7 +2,7 @@ package com.kero.security.lang.provider;
 
 import com.kero.security.lang.collections.RootNodeList;
 
-public class ProviderCacheWrap implements KsdlProvider {
+public class ProviderCacheWrap implements KsdlProvider, PreloadableProvider {
 
 	private KsdlProvider original;
 	
@@ -22,5 +22,14 @@ public class ProviderCacheWrap implements KsdlProvider {
 		}
 		
 		return this.roots;
+	}
+
+	@Override
+	public void preloadResource() {
+		
+		if(original instanceof PreloadableProvider) {
+			
+			((PreloadableProvider) original).preloadResource();
+		}
 	}
 }
--- src/test/java/com/kero/security/core/agent/KeroAccessAgentFactoryImplTest.java
+++ src/test/java/com/kero/security/core/agent/KeroAccessAgentFactoryImplTest.java
@@ -4,19 +4,42 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import com.kero.security.core.agent.configuration.KeroAccessAgentConfigurator;
+import com.kero.security.lang.provider.KsdlProvider;
+import com.kero.security.lang.provider.TextualProvider;
 
 public class KeroAccessAgentFactoryImplTest {
 
 	@Test
 	public void test() {
 		
-		KeroAccessAgentConfigurator configigurator = Mockito.mock(KeroAccessAgentConfigurator.class);
+		KeroAccessAgentConfigurator configurator = Mockito.mock(KeroAccessAgentConfigurator.class);
 		
 		KeroAccessAgentFactoryImpl factory = new KeroAccessAgentFactoryImpl();
-			factory.addConfigurator(configigurator);
+			factory.addConfigurator(configurator);
 	
 		factory.create();
 			
-		Mockito.verify(configigurator, Mockito.times(1)).configure(Mockito.any());
+		Mockito.verify(configurator, Mockito.times(1)).configure(Mockito.any());
+	}
+	
+	@Test
+	public void testPreload() {
+		
+		TextualProvider provider = Mockito.mock(TextualProvider.class);
+		
+		KeroAccessAgentFactory factory = new KeroAccessAgentFactoryImpl.Builder().setMainProviderPreloading(true).build();
+		
+		factory.addConfigurator(new KeroAccessAgentConfigurator() {
+			
+			@Override
+			public void configure(KeroAccessAgent agent) {
+				
+				agent.addKsdlProvider(provider);
+			}
+		});
+		
+		KeroAccessAgent agent = factory.create();
+	
+		Mockito.verify(provider, Mockito.times(1)).preloadResource();
 	}
 }
